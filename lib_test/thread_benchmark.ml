60eb506c823129f039f4a6b1b2c1288c5a655e52
David Scott <dave@recoil.org>
2017-01-15 11:18:14 +0000

    test: add a thread communication benchmark
    Signed-off-by: David Scott <dave.scott@docker.com>

---
+++ lib_test/thread_benchmark.ml
@@ -0,0 +1,27 @@
+module Benchmark(Fn: Lwt_hvsock.FN) = struct
+
+  let run () =
+    let counter = ref 0 in
+    let f = Fn.create (fun () -> incr counter) in
+    let iterations = 1_000_000 in
+    let start = Unix.gettimeofday () in
+    Lwt_main.run begin
+      let open Lwt.Infix in
+      let rec loop = function
+        | 0 -> Lwt.return_unit
+        | n ->
+          Fn.fn f ()
+          >>= fun () ->
+          loop (n - 1) in
+      loop iterations
+    end;
+    Alcotest.(check int) "Iterations" iterations (!counter);
+    let per_sec = float_of_int iterations /. (Unix.gettimeofday () -. start) in
+    per_sec
+end
+
+module Run_in_thread = Benchmark(Lwt_hvsock.Run_in_thread(Lwt_preemptive))
+
+let () =
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Printf.printf "Run_in_thread: %.1f calls per second\n" (Run_in_thread.run ())
