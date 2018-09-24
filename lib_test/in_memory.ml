9d583a1185b2e36741f04620b5639145de7d7e68
David Scott <dave@recoil.org>
2018-09-24 21:25:13 +0100

    test: add an in-memory loopback interface
    Signed-off-by: David Scott <dave@recoil.org>

---
+++ lib_test/in_memory.ml
@@ -0,0 +1,136 @@
+type sockaddr = unit
+
+type queues =
+  {read: Cstruct.t list ref; write: Cstruct.t list ref; mutable shutdown: bool}
+
+type listening = {mutable connecting: t list; sockaddr: sockaddr}
+
+and state =
+  | Connected of queues
+  | Listening of listening
+  | Created
+  | Bound of sockaddr
+  | Closed
+
+and t = {mutable state: state; m: Mutex.t; c: Condition.t}
+
+let string_of_sockaddr () = "()"
+
+let create () = {state= Created; m= Mutex.create (); c= Condition.create ()}
+
+let bound_sockets = Hashtbl.create 7
+
+let with_lock m f =
+  Mutex.lock m ;
+  try
+    let r = f () in
+    Mutex.unlock m ; r
+  with e -> Mutex.unlock m ; raise e
+
+let bind t sockaddr =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Created ->
+          if Hashtbl.mem bound_sockets sockaddr then
+            raise (Unix.Unix_error (Unix.EADDRINUSE, "bind", "")) ;
+          Hashtbl.replace bound_sockets sockaddr t ;
+          t.state <- Bound sockaddr
+      | _ -> raise (Unix.Unix_error (Unix.EINVAL, "accept", "")) )
+
+let listen t =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Bound sockaddr -> t.state <- Listening {connecting= []; sockaddr}
+      | _ -> raise (Unix.Unix_error (Unix.EINVAL, "listen", "")) )
+
+let accept t =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Listening l ->
+          let rec wait () =
+            match l.connecting with
+            | [] -> Condition.wait t.c t.m ; wait ()
+            | x :: xs ->
+                l.connecting <- xs ;
+                let qs = {read= ref []; write= ref []; shutdown= false} in
+                with_lock x.m (fun () ->
+                    x.state <- Connected qs ;
+                    Condition.broadcast x.c ) ;
+                let y =
+                  { x with
+                    state=
+                      Connected
+                        {read= qs.write; write= qs.read; shutdown= false} }
+                in
+                (y, ())
+          in
+          let result = wait () in
+          result
+      | _ -> raise (Unix.Unix_error (Unix.EINVAL, "accept", "")) )
+
+let connect ?timeout_ms:_ t sockaddr =
+  with_lock t.m (fun () ->
+      if Hashtbl.mem bound_sockets sockaddr then (
+        let listening = Hashtbl.find bound_sockets sockaddr in
+        with_lock listening.m (fun () ->
+            match listening.state with
+            | Listening l ->
+                l.connecting <- t :: l.connecting ;
+                Condition.signal listening.c
+            | _ -> assert false ) ;
+        let rec wait () =
+          match t.state with
+          | Connected _ -> ()
+          | _ -> Condition.wait t.c t.m ; wait ()
+        in
+        wait () )
+      else raise (Unix.Unix_error (Unix.ECONNREFUSED, "connect", "")) )
+
+let writev t bufs =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Connected qs ->
+          if qs.shutdown then
+            raise (Unix.Unix_error (Unix.EPIPE, "writev", "")) ;
+          qs.write := !(qs.write) @ bufs ;
+          Condition.signal t.c ;
+          List.fold_left ( + ) 0 (List.map Cstruct.len bufs)
+      | _ -> raise (Unix.Unix_error (Unix.ENOTCONN, "writev", "")) )
+
+let read_into t buf =
+  with_lock t.m (fun () ->
+      let rec read () =
+        match t.state with
+        | Connected qs -> (
+          match !(qs.read) with
+          | [] ->
+              if qs.shutdown then 0 else ( Condition.wait t.c t.m ; read () )
+          | b :: bs ->
+              let buf_len = Cstruct.len buf in
+              let b_len = Cstruct.len b in
+              let to_read = min b_len buf_len in
+              Cstruct.blit b 0 buf 0 to_read ;
+              if buf_len >= b_len then qs.read := bs
+              else qs.read := Cstruct.shift b to_read :: bs ;
+              to_read )
+        | _ -> raise (Unix.Unix_error (Unix.ENOTCONN, "read_into", ""))
+      in
+      read () )
+
+let shutdown_write t =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Connected qs ->
+          qs.shutdown <- true ;
+          Condition.broadcast t.c
+      | _ -> raise (Unix.Unix_error (Unix.EINVAL, "shutdown_write", "")) )
+
+let close t =
+  with_lock t.m (fun () ->
+      match t.state with
+      | Connected qs ->
+          t.state <- Closed ;
+          Condition.broadcast t.c
+      | Bound sockaddr -> Hashtbl.remove bound_sockets sockaddr
+      | Listening {sockaddr; _} -> Hashtbl.remove bound_sockets sockaddr
+      | _ -> raise (Unix.Unix_error (Unix.EINVAL, "close", "")) )
--- lib_test/test.ml
+++ lib_test/test.ml
@@ -1,11 +1,51 @@
-let test_something_case () = ()
+module In_memory_buffer =
+  Hvsock_lwt.Buffering.Make (Hvsock_lwt_unix.Preemptive_detach) (In_memory)
 
-let test_something = [
-  "something", `Quick, test_something_case;
-]
+let test_accept_connect () =
+  let server = In_memory.create () in
+  In_memory.bind server () ;
+  In_memory.listen server ;
+  let server_thread =
+    Thread.create
+      (fun () ->
+        let sock, _ = In_memory.accept server in
+        In_memory.close sock )
+      ()
+  in
+  let client = In_memory.create () in
+  In_memory.connect client () ;
+  Thread.join server_thread ;
+  In_memory.close server
+
+let test_write_read () =
+  let server = In_memory.create () in
+  In_memory.bind server () ;
+  In_memory.listen server ;
+  let received = ref "" in
+  let server_thread =
+    Thread.create
+      (fun () ->
+        let sock, _ = In_memory.accept server in
+        let buf = Cstruct.create 10 in
+        let n = In_memory.read_into sock buf in
+        (received := Cstruct.(to_string @@ sub buf 0 n)) ;
+        In_memory.close sock )
+      ()
+  in
+  let client = In_memory.create () in
+  In_memory.connect client () ;
+  let n = In_memory.writev client [Cstruct.of_string "hello\n"] in
+  Alcotest.(check int) "writev" 6 n;
+  In_memory.shutdown_write client ;
+  Thread.join server_thread ;
+  Alcotest.(check string) "read_into" "hello\n" !received;
+  In_memory.close server ;
+  In_memory.close client
+
+let test_simulated_socket =
+  [ ("accept connect", `Quick, test_accept_connect)
+  ; ("write then read", `Quick, test_write_read) ]
 
 let () =
-  Logs.set_reporter (Logs_fmt.reporter ());
-  Alcotest.run "hvsock" [
-    "something", test_something;
-  ]
+  Logs.set_reporter (Logs_fmt.reporter ()) ;
+  Alcotest.run "hvsock" [("socket", test_simulated_socket)]
